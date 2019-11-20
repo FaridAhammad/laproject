592475d5a5fdfebe45325587a314006bd0466ee4
Sarifit95 <33935723+Sarifit95@users.noreply.github.com>
2019-11-20 15:14:48 +0600

    upload Product_sub_group Model
    upload Product_sub_group Model

---
+++ app/model/Product_sub_group.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product_sub_group extends Model
+{
+    public $timestamps=false;
+    protected $table = 'item_sub_group';
+    protected $guarded = [];
+    protected $fillable = [
+        'sub_group_id',
+        'sub_group_name',
+        'group_id',
+
+
+
+
+
+    ];
+
+
+    public function product_group(){
+
+        return $this->belongsTo(Product_group::class, 'group_id', 'group_id')->where('status',0);
+
+
+    }
+
+}

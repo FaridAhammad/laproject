5c61bae510eac0cd51d1c1dc68c63069a93c9406
Mohiuuddin <41298905+Mohiuuddin@users.noreply.github.com>
2019-12-10 15:53:20 +0600

    Update Warehouse_other_receive_detail.php

--- app/model/Warehouse_other_receive_detail.php
+++ app/model/Warehouse_other_receive_detail.php
@@ -11,9 +11,19 @@ class Warehouse_other_receive_detail extends Model
     protected $guarded = [];
     protected $fillable = [
         'or_no',
+        'item_id',
+        'vendor_no',
         'or_date',
         'vendor_name',
         'receive_type',
+        'warehouse_id',
+        'unit_name',
+        'rate',
+        'qty',
+        'discount',
+        'amount',
+        'entry_by',
+        'entry_at',
 
 
 

f0cbb938c634c7e1b1b355e367cd06fa87cc8ef2
Mohiuuddin <41298905+Mohiuuddin@users.noreply.github.com>
2020-01-10 15:00:00 +0600

    Update AuthGates.php

--- app/Http/Middleware/AuthGates.php
+++ app/Http/Middleware/AuthGates.php
@@ -12,21 +12,21 @@ class AuthGates
     {
         $user = \Auth::user();
 
-        if (!app()->runningInConsole() && $user) {
-            $roles = Role::with('permissions')->get();
+//         if (!app()->runningInConsole() && $user) {
+//             $roles = Role::with('permissions')->get();
 
-            foreach ($roles as $role) {
-                foreach ($role->permissions as $permissions) {
-                    $permissionsArray[$permissions->title][] = $role->id;
-                }
-            }
+//             foreach ($roles as $role) {
+//                 foreach ($role->permissions as $permissions) {
+//                     $permissionsArray[$permissions->title][] = $role->id;
+//                 }
+//             }
 
-            foreach ($permissionsArray as $title => $roles) {
-                Gate::define($title, function (\App\User $user) use ($roles) {
-                    return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
-                });
-            }
-        }
+//             foreach ($permissionsArray as $title => $roles) {
+//                 Gate::define($title, function (\App\User $user) use ($roles) {
+//                     return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
+//                 });
+//             }
+//         }
 
         return $next($request);
     }
